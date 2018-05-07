db17d44fab63879ecec13955ec2d640985fd8bc7
Boiko Andrei <BoikoAndrei1996@gmail.com>
2018-05-07 22:29:00 +0300

    add function create new order.

--- YourMotivation.Web/Controllers/OrderController.cs
+++ YourMotivation.Web/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using ORM.Models;
@@ -57,12 +58,57 @@ namespace YourMotivation.Web.Controllers
         ViewBag.OwnerUsername = $" {_localizer["by"].Value} {user.UserName}";
       }
 
-      OrdersPageViewModel page = 
+      OrdersPageViewModel page =
         await _orderManager.GetOrderPageAsync(pageIndex ?? 1, 4, sortOrder ?? SortState.CreatedDateDesc, userId);
 
       page.StatusMessage = this.StatusMessage;
 
       return View(page);
+    }
+
+    // GET Order/Details
+    public async Task<IActionResult> Details(Guid? cartId)
+    {
+      if (!cartId.HasValue)
+      {
+        return NotFound();
+      }
+
+      var orderItems = await _orderManager.GetOrderItemsAsync(cartId.Value);
+
+      return View(orderItems);
+    }
+
+    // POST Order/Create
+    [HttpPost]
+    [ActionName("Create")]
+    public async Task<IActionResult> CreateNewOrder(Guid? userId)
+    {
+      if (!userId.HasValue)
+      {
+        return NotFound();
+      }
+
+      var result = await _orderManager.CreateNewOrderAsync(userId.Value);
+      if (result == null)
+      {
+        return NotFound();
+      }
+      else if (result.Succeeded)
+      {
+        this.StatusMessage = _localizer["Success: order has beed created."];
+        return RedirectToAction(nameof(OrderController.All), "Order");
+      }
+      else
+      {
+        this.StatusMessage = result.Errors.Single().Description;
+      }
+
+      var user = await _userManager.Users
+        .Include(u => u.Cart)
+        .FirstAsync(u => u.UserName == User.Identity.Name);
+
+      return RedirectToAction("Cart", "Shop", new { id = user.Cart.Id });
     }
 
     // POST Order/Close
@@ -75,18 +121,18 @@ namespace YourMotivation.Web.Controllers
         return NotFound();
       }
 
-      var isSuccess = await _orderManager.CloseOrderAsync(orderId.Value);
-      if (!isSuccess.HasValue)
+      var result = await _orderManager.CloseOrderAsync(orderId.Value);
+      if (result == null)
       {
         return NotFound();
       }
-      else if (isSuccess.Value)
+      else if (result.Succeeded)
       {
         this.StatusMessage = _localizer["Success: order has beed closed."];
       }
       else
       {
-        this.StatusMessage = _localizer["Error: something has gone wrong while closing order."];
+        this.StatusMessage = result.Errors.Single().Description;
       }
 
       return RedirectToAction(nameof(OrderController.All));
@@ -101,34 +147,21 @@ namespace YourMotivation.Web.Controllers
         return NotFound();
       }
 
-      var isSuccess = await _orderManager.RemoveOrderAsync(orderId.Value);
-      if (!isSuccess.HasValue)
+      var result = await _orderManager.RemoveOrderAsync(orderId.Value);
+      if (result == null)
       {
         return NotFound();
       }
-      else if (isSuccess.Value)
+      else if (result.Succeeded)
       {
         this.StatusMessage = _localizer["Success: order has beed remove."];
       }
       else
       {
-        this.StatusMessage = _localizer["Error: something has gone wrong while removing order."];
+        this.StatusMessage = result.Errors.Single().Description;
       }
 
       return RedirectToAction(nameof(OrderController.All));
-    }
-
-    // GET Order/Details
-    public async Task<IActionResult> Details(Guid? cartId)
-    {
-      if (!cartId.HasValue)
-      {
-        return NotFound();
-      }
-
-      var orderItems = await _orderManager.GetOrderItemsAsync(cartId.Value);
-
-      return View(orderItems);
     }
   }
 }
--- YourMotivation.Web/Services/OrderManager.cs
+++ YourMotivation.Web/Services/OrderManager.cs
@@ -7,17 +7,23 @@ using ORM;
 using YourMotivation.Web.Models.OrderViewModels;
 using YourMotivation.Web.Models.Pagination;
 using YourMotivation.Web.Models.Pagination.Pages;
+using ORM.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
 
 namespace YourMotivation.Web.Services
 {
   public class OrderManager
   {
     private readonly ApplicationDbContext _context;
+    private readonly IStringLocalizer<OrderManager> _localizer;
 
     public OrderManager(
-      ApplicationDbContext context)
+      ApplicationDbContext context,
+      IStringLocalizer<OrderManager> localizer)
     {
       _context = context;
+      _localizer = localizer;
     }
 
     public async Task<OrdersPageViewModel> GetOrderPageAsync(
@@ -68,56 +74,6 @@ namespace YourMotivation.Web.Services
       return result;
     }
 
-    public async Task<bool?> CloseOrderAsync(Guid orderId)
-    {
-      var order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == orderId);
-      if (order == null)
-      {
-        return null;
-      }
-
-      if (order.DateOfClosing.HasValue)
-      {
-        return true;
-      }
-
-      try
-      {
-        order.DateOfClosing = DateTime.UtcNow;
-
-        _context.Orders.Update(order);
-        await _context.SaveChangesAsync();
-
-        return true;
-      }
-      catch (Exception)
-      {
-        return false;
-      }
-    }
-
-    public async Task<bool?> RemoveOrderAsync(Guid orderId)
-    {
-      var order = await _context.Orders.Include(o => o.Cart).FirstOrDefaultAsync(o => o.Id == orderId);
-      if (order == null)
-      {
-        return null;
-      }
-
-      try
-      {
-        _context.Carts.Remove(order.Cart);
-        _context.Orders.Remove(order);
-        await _context.SaveChangesAsync();
-
-        return true;
-      }
-      catch (Exception)
-      {
-        return false;
-      }
-    }
-
     public async Task<IList<OrderItemViewModel>> GetOrderItemsAsync(Guid cartId)
     {
       var cart = await _context.Carts
@@ -127,12 +83,185 @@ namespace YourMotivation.Web.Services
         .FirstOrDefaultAsync(c => c.Id == cartId);
 
       var items = new List<OrderItemViewModel>();
-      foreach(var cartItem in cart.CartItems)
+      foreach (var cartItem in cart.CartItems)
       {
         items.Add(OrderItemViewModel.Map(cartItem.Item, cartItem.Count));
       }
 
       return items;
+    }
+
+    public async Task<IdentityResult> CloseOrderAsync(Guid orderId)
+    {
+      var order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == orderId);
+      if (order == null)
+      {
+        return null;
+      }
+
+      if (order.DateOfClosing.HasValue)
+      {
+        return IdentityResult.Success;
+      }
+
+      try
+      {
+        order.DateOfClosing = DateTime.UtcNow;
+
+        _context.Orders.Update(order);
+        await _context.SaveChangesAsync();
+
+        return IdentityResult.Success;
+      }
+      catch (DbUpdateException ex)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CloseOrderAsync),
+          Description = ex.InnerException.Message
+        });
+      }
+      catch (Exception)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CloseOrderAsync),
+          Description = _localizer["Error: something has gone wrong while closing order."]
+        });
+      }
+    }
+
+    public async Task<IdentityResult> RemoveOrderAsync(Guid orderId)
+    {
+      var order = await _context.Orders.Include(o => o.Cart).FirstOrDefaultAsync(o => o.Id == orderId);
+      if (order == null)
+      {
+        return null;
+      }
+
+      if (order.DateOfClosing.HasValue)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.RemoveOrderAsync),
+          Description = _localizer["Error: You can not remove closed order."]
+        });
+      }
+
+      try
+      {
+        _context.Carts.Remove(order.Cart);
+        _context.Orders.Remove(order);
+        await _context.SaveChangesAsync();
+
+        return IdentityResult.Success;
+      }
+      catch (DbUpdateException ex)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.RemoveOrderAsync),
+          Description = ex.InnerException.Message
+        });
+      }
+      catch (Exception)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.RemoveOrderAsync),
+          Description = _localizer["Error: something has gone wrong while removing order."]
+        });
+      }
+    }
+
+    public async Task<IdentityResult> CreateNewOrderAsync(Guid userId)
+    {
+      var user = await _context.Users
+        .Include(u => u.Cart)
+          .ThenInclude(c => c.CartItems)
+            .ThenInclude(ci => ci.Item)
+        .FirstOrDefaultAsync(u => u.Id == userId);
+
+      if (user == null)
+      {
+        return null;
+      }
+
+      var cartPoints = user.Cart.CartItems.Sum(ci => ci.Item.Price * ci.Count);
+
+      var checkResult = this.CheckOrderIsPossible(user, cartPoints);
+      if (!checkResult.Succeeded)
+      {
+        return checkResult;
+      }
+
+      user.TotalPoints -= cartPoints;
+      foreach (var cartItem in user.Cart.CartItems)
+      {
+        cartItem.Item.CountsInStock -= cartItem.Count;
+      }
+
+      var oldCart = user.Cart;
+      oldCart.UserId = null;
+      user.Cart = new Cart();
+
+      var order = new Order
+      {
+        CartId = oldCart.Id,
+        UserId = user.Id,
+        DateOfCreation = DateTime.UtcNow
+      };
+
+      try
+      {
+        _context.Carts.Update(oldCart);
+        _context.Users.Update(user);
+        await _context.Orders.AddAsync(order);
+
+        await _context.SaveChangesAsync();
+
+        return IdentityResult.Success;
+      }
+      catch(DbUpdateException ex)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CreateNewOrderAsync),
+          Description = ex.InnerException.Message
+        });
+      }
+      catch(Exception)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CreateNewOrderAsync),
+          Description = _localizer["Error: something has gone wrong while creating order."]
+        });
+      }
+    }
+
+    private IdentityResult CheckOrderIsPossible(ApplicationUser user, int cartPoints)
+    {
+      var cartItem = user.Cart.CartItems.FirstOrDefault(ci => ci.Count > ci.Item.CountsInStock);
+      if (cartItem != null)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CreateNewOrderAsync),
+          Description = _localizer["Error: there are not enough items '{0}' in stock.", cartItem.Item.Title]
+        });
+      }
+
+      if (cartPoints > user.TotalPoints)
+      {
+        return IdentityResult.Failed(new IdentityError
+        {
+          Code = nameof(OrderManager.CreateNewOrderAsync),
+          Description = _localizer["Error: you have not enough points."]
+        });
+      }
+
+      return IdentityResult.Success;
     }
   }
 }
